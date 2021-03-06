cfa53b25e1b945b795ef76d90a6d28633b7bfede
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2021-03-06 11:13:58 -0600

    added spinner element

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 
 /*
@@ -10,6 +12,8 @@ import android.widget.TextView
  * view the result on the screen
  */
 class MainActivity : AppCompatActivity() {
+
+
     // onCreate() is basically android's main() function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +23,25 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         // "listens" for the user to click on the button
         rollButton.setOnClickListener { rollDice() }
+
+        // reference to the spinner dropdown menu
+        val spinner: Spinner = findViewById(R.id.spinner)
+
+        // Create an array adapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.dice_array,
+            android.R.layout.simple_spinner_item
+        ).also {adapter ->
+            // specify the layout to use when the list of dice appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
+
+
+
 
     /*
      * Roll the dice and update the screen with the result.
