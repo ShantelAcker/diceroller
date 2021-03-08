8d2a26e371e25ef219fdb3ce9116b10b85f650a8
ShantelAcker <56419079+shantelacker@users.noreply.github.com>
2021-03-08 05:16:56 -0600

    added dice images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -23,20 +23,6 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         // "listens" for the user to click on the button
         rollButton.setOnClickListener { rollDice() }
-
-        // reference to the spinner dropdown menu
-        val spinner: Spinner = findViewById(R.id.spinner)
-
-        // Create an array adapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.dice_array,
-            android.R.layout.simple_spinner_item
-        ).also {adapter ->
-            // specify the layout to use when the list of dice appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // apply the adapter to the spinner
-            spinner.adapter = adapter
         }
     }
 
@@ -47,6 +33,9 @@ class MainActivity : AppCompatActivity() {
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
+        // based on selection in spinner, set the number of sides the dice should have
+        
+
         // create a dice with 6 sides and roll it
         // saving the result in a variable
         val dice = Dice(6)
